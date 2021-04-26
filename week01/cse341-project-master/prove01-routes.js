7395c4d5a4dbd650571a9e633624507e1706c433
abrower10612 <abrower7@byui.edu>
2021-04-25 20:35:54 -0600

    Adding user list feature

--- week01/cse341-project-master/prove01-routes.js
+++ week01/cse341-project-master/prove01-routes.js
@@ -1,30 +1,74 @@
 const fs = require('fs');
 
+const userList = [];
+
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
+
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><h1>Welcome!</h1><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</form></body>');
+    res.write(
+      '<body>' 
+      + '<h1>Welcome!</h1>'
+      + '<form action="/create-user" method="POST">'
+        + '<input type="text" name="username">'
+        + '<button type="submit">Send</button>'
+      + '</form>'
+      + '<form action="/user-list" method="POST">'
+        + '<button type="submit">See User List</button>'
+      + '</form>'
+      + '</body>');
     res.write('</html>');
     return res.end();
   }
+
   if (url === '/create-user' && method === 'POST') {
     const body = [];
     req.on('data', (chunk) => {
       body.push(chunk);
     });
+
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
       console.log(message);
-      fs.writeFile('message.txt', message, err => {
+      userList.push(message);
+      fs.appendFile('message.txt', message + '\n', err => {
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
       });
     });
+  }
+
+  if (url === '/user-list' && method === 'POST') {
+      fs.readFile('./message.txt', 'utf8', (err, data) => {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        if (data.length == 0) {
+          res.write(
+            '<body>' 
+              + '<h1>'
+              + 'No users found'
+              + '</h1>'
+            + '</body>');
+        }
+        else {
+          res.write(
+            '<body>' 
+              + '<h1>')
+              for (user of userList) {
+                res.write(user);
+                res.write('<br>');
+              }
+          res.write('</h1>'
+            + '</body>');
+        }
+        res.write('</html>');
+        return res.end();
+      });
   }
 };
 
