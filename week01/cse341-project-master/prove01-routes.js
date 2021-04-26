1c14c4eedfcb57496c8a7c025bfe5f61c6c7a040
abrower10612 <abrower7@byui.edu>
2021-04-25 19:04:05 -0600

    Add console.log of message upon submission

--- week01/cse341-project-master/prove01-routes.js
+++ week01/cse341-project-master/prove01-routes.js
@@ -13,12 +13,12 @@ const requestHandler = (req, res) => {
   if (url === '/create-user' && method === 'POST') {
     const body = [];
     req.on('data', (chunk) => {
-      console.log(chunk);
       body.push(chunk);
     });
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
+      console.log(message);
       fs.writeFile('message.txt', message, err => {
         res.statusCode = 302;
         res.setHeader('Location', '/');
@@ -26,12 +26,12 @@ const requestHandler = (req, res) => {
       });
     });
   }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from my Node.js Server</body>');
-  res.write('</html>');
-  res.end();
+  // res.setHeader('Content-Type', 'text/html');
+  // res.write('<html>');
+  // res.write('<head><title>My First Page</title><head>');
+  // res.write('<body><h1>Hello from my Node.js Server</body>');
+  // res.write('</html>');
+  // res.end();
 };
 
 module.exports = requestHandler;
